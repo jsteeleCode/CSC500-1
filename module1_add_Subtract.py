d9eadbfa42fbc7a6d9ab4949ddebdf4b5ffcf95f
jsteeleCode <51207733+jsteeleCode@users.noreply.github.com>
2026-01-18 09:55:16 -0700

    Update module1_add_Subtract.py

--- module1_add_Subtract.py
+++ module1_add_Subtract.py
@@ -11,3 +11,18 @@ subtract_result = num1 - num2
 #display the added and subtracted results
 print("The sum of", num1, "and", num2, "is:", sum_result)
 print("when", num2, "is subtracted from", num1, "you get:", subtract_result)
+
+#now ask the user for two more numbers
+num3 = float(input("Enter the third number: "))
+num4 = float(input("Enter the fourth number: "))
+
+#multiply the two numbers
+multiply_result = num3 * num4
+#divide the two numbers
+divide_result = num3 / num4
+
+#display the multiplied and divided results
+print(num3, "times", num4, "is:", multiply_result)
+print("when", num3, "is divided by", num4, "you get:", divide_result)
+
+#end of program
