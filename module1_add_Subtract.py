d8f3400e6fc1b0ee19ac76e254f9d99c05abf362
jsteeleCode <51207733+jsteeleCode@users.noreply.github.com>
2026-01-18 09:46:20 -0700

    Add files via upload

---
+++ module1_add_Subtract.py
@@ -0,0 +1,13 @@
+#ask user for a number, then another number
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+#add the two numbers
+sum_result = num1 + num2
+
+#subtract the numbers
+subtract_result = num1 - num2   
+
+#display the added and subtracted results
+print("The sum of", num1, "and", num2, "is:", sum_result)
+print("when", num2, "is subtracted from", num1, "you get:", subtract_result)
